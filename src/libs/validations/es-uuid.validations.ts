0e8afce20c04a0eeeaefbba7cb08d98ea8853f06
Xtian <xtiancuenta1@gmail.com>
2023-03-03 20:08:32 -0300

    EsUuid creada

---
+++ src/libs/validations/es-uuid.validations.ts
@@ -0,0 +1,10 @@
+
+export function EsUuid(valor: string): boolean {
+
+    const regex =
+        /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/i;
+
+    const matches = valor.match(regex);
+
+    return matches != null ? true : false;
+}
