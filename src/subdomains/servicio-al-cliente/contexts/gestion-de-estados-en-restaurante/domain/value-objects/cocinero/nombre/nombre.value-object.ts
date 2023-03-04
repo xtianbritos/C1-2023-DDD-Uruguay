ee4614c71660c12123b9a55adb08a1056f27d4f3
Xtian <xtiancuenta1@gmail.com>
2023-03-04 09:47:47 -0300

    Objeto de valor nombre hecho y validado

---
+++ src/subdomains/servicio-al-cliente/contexts/gestion-de-estados-en-restaurante/domain/value-objects/cocinero/nombre/nombre.value-object.ts
@@ -0,0 +1,79 @@
+import { EsNombre, LongitudMaxima, LongitudMinima } from '../../../../../../../../libs/validations';
+import { ValueObjectBase } from '../../../../../../../../libs/sofka/bases';
+import { IErrorValueObject } from '../../../../../../../../libs/sofka/interface';
+
+
+export class NombreValueObject extends ValueObjectBase<string> {
+
+    constructor(value: string) {
+        super(value);
+    }
+    
+    /**
+     *Funsión que realiza todas las validaciones
+     *
+     * @memberof NombreValueObject
+     */
+    validateData(): void {
+        this.validarEstructura();
+        this.validarLongitudMinima(5);
+        this.validarLongitudMaxima(50);
+    }
+
+    /**
+     *Funsión que valida la estructura de un nombre
+     *
+     * @private
+     * @memberof NombreValueObject
+     */
+    private validarEstructura(): void {
+        if(EsNombre(this.value) === false) {
+
+            const error: IErrorValueObject = {
+                field: 'Nombre',
+                message: 'El nombre no es válido'
+            };
+
+            this.setError(error);
+        }
+    }
+
+    /**
+     *Funsión que valida la longitud mínima de un nombre
+     *
+     * @private
+     * @param {number} longitud
+     * @memberof NombreValueObject
+     */
+    private validarLongitudMinima(longitud: number): void {
+        if(LongitudMinima(this.value, longitud) === false) {
+
+            const error: IErrorValueObject = {
+                field: 'Nombre',
+                message: 'El nombre debe tener una longitud mínima de '+longitud+' caracteres'
+            };
+
+            this.setError(error);
+        }
+    }
+
+    /**
+     *Funsión que valida la longitud máxima de un nombre
+     *
+     * @private
+     * @param {number} longitud
+     * @memberof NombreValueObject
+     */
+     private validarLongitudMaxima(longitud: number): void {
+        if(LongitudMaxima(this.value, longitud) === false) {
+
+            const error: IErrorValueObject = {
+                field: 'Nombre',
+                message: 'El nombre debe tener una longitud máxima de '+longitud+' caracteres'
+            };
+
+            this.setError(error);
+        }
+    }
+    
+}
