64e56fcc2c493a5ae1d13b4aecbae70dd5eece37
Xtian <xtiancuenta1@gmail.com>
2023-03-04 14:34:53 -0300

    Objetos de valor de orden completados y validados

--- src/libs/validations/index.ts
+++ src/libs/validations/index.ts
@@ -1,6 +1,7 @@
 export * from './es-boolean.validations';
 export * from './es-estado-orden.validations';
 export * from './es-nombre.validations';
+export * from './es-string-alfabetico.validations';
 export * from './es-uuid.validations';
 export * from './longitud-maxima.validations';
 export * from './longitud-minima.validations';
