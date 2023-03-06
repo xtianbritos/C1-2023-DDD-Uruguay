467e306146a33209f6a201770f4ebebe2f93a796
Xtian <xtiancuenta1@gmail.com>
2023-03-06 10:54:01 -0300

    Se agregaron comentarios a la interfaz y la clase base de cuenta

--- src/subdomains/servicio-al-cliente/contexts/gestion-de-estados-en-restaurante/domain/entities/cuenta.domain-entity.ts
+++ src/subdomains/servicio-al-cliente/contexts/gestion-de-estados-en-restaurante/domain/entities/cuenta.domain-entity.ts
@@ -3,6 +3,13 @@ import { ICuentaDomainEntity } from "./interfaces";
 
 import { v4 as uuid } from 'uuid';
 
+/**
+ *Clase base de entidad Cuenta
+ *
+ * @export
+ * @class CuentaDomainEntityBase
+ * @implements {ICuentaDomainEntity}
+ */
 export class CuentaDomainEntityBase implements ICuentaDomainEntity {
     cuentaId?: string | CuentaIdValueObject;
     pagoFueRecibido?: boolean | PagoFueRecibidoValueObject;
@@ -17,7 +24,7 @@ export class CuentaDomainEntityBase implements ICuentaDomainEntity {
         if(_data.pagoFueRecibido) this.pagoFueRecibido = _data.pagoFueRecibido;
 
         if(_data.vueltoFueEntregado) this.vueltoFueEntregado = _data.vueltoFueEntregado;
-        
+
         if(_data.cuentaFueEntregada) this.cuentaFueEntregada = _data.cuentaFueEntregada;
 
         this.createdAt = new Date();
--- src/subdomains/servicio-al-cliente/contexts/gestion-de-estados-en-restaurante/domain/entities/interfaces/cuenta.domain-entity.interface.ts
+++ src/subdomains/servicio-al-cliente/contexts/gestion-de-estados-en-restaurante/domain/entities/interfaces/cuenta.domain-entity.interface.ts
@@ -1,5 +1,11 @@
 import { CuentaIdValueObject, FueEntregadaValueObject, PagoFueRecibidoValueObject, VueltoFueEntregadoValueObject } from '../../value-objects/mesero/cuenta';
 
+/**
+ *Interfaz para la entidad Cuenta
+ *
+ * @export
+ * @interface ICuentaDomainEntity
+ */
 export interface ICuentaDomainEntity {
     cuentaId?: string | CuentaIdValueObject;
     pagoFueRecibido?: boolean | PagoFueRecibidoValueObject;
