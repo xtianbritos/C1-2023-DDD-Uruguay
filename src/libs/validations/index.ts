705380445d70674ae8fc5b536c2bdca7fa916ddd
Xtian <xtiancuenta1@gmail.com>
2023-03-06 22:25:17 -0300

    Se agregaron los objetos de valor restantes de la entidad cliente

--- src/libs/validations/index.ts
+++ src/libs/validations/index.ts
@@ -3,6 +3,7 @@ export * from './es-boolean.validations';
 export * from './es-estado-mesa.validations';
 export * from './es-estado-orden.validations';
 export * from './es-nombre.validations';
+export * from './es-direccion.validations';
 export * from './es-number-valido.validations';
 export * from './es-string-alfabetico.validations';
 export * from './es-uuid.validations';
