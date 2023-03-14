9588733b5bd9329dab339b626bbe588fc446b677
Xtian <xtiancuenta1@gmail.com>
2023-03-14 14:31:48 -0300

    Se crearon los messaging publisher restantes para ela gregado Tickets

---
+++ src/subdomains/servicio-al-cliente/contexts/entrega-a-domicilio/infrastructure/messaging/publisher/ticket/vehiculo-repartidor-cambiado.message-publisher.ts
@@ -0,0 +1,25 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+import { IEventPublisher } from 'src/libs/sofka';
+import { VehiculoRepartidorCambiadoEventPublisherBase } from '../../../../domain/events/publishers/ticket';
+import { RepartidorEntity } from '../../../persistence/entities';
+
+
+@Injectable()
+export class VehiculoRepartidorCambiadoPublisher extends VehiculoRepartidorCambiadoEventPublisherBase {
+    constructor(@Inject('ENTREGA-A-DOMICILIO_CONTEXT') private readonly proxy: ClientProxy) {
+        super(proxy as unknown as IEventPublisher);
+    }
+
+    // send<Result, Input = UserEntity>(pattern: any, data: Input): Promise<Result> {
+    //   return lastValueFrom<Result>(this.proxy.send(pattern, data));
+    // }
+
+    emit<Result = any, Input = RepartidorEntity>(
+        pattern: any,
+        data: Input,
+    ): Promise<Result> {
+        return lastValueFrom<Result>(this.proxy.emit(pattern, data));
+    }
+}
